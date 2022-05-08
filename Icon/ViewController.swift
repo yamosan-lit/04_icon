7f0cc5e0b60c5dfe3477ff378493c0c937672fa4
yamosan <yamorimasato@gmail.com>
2022-05-09 00:02:07 +0900

    Initial commit

---
+++ Icon/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Icon
+//
+//  Created by 八森聖人 on 2022/05/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
